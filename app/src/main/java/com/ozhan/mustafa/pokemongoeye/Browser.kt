7061713df0af9d020ea2e16f990616daa0d2f671
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2017-11-26 23:41:52 +0300

    Concise Codes

--- app/src/main/java/com/ozhan/mustafa/pokemongoeye/Browser.kt
+++ app/src/main/java/com/ozhan/mustafa/pokemongoeye/Browser.kt
@@ -2,16 +2,13 @@ package com.ozhan.mustafa.pokemongoeye
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.webkit.WebView
+import kotlinx.android.synthetic.main.activity_browser.*
 
 class Browser : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_browser)
-        val wv = findViewById(R.id.webw) as WebView
-
-
-        wv.loadUrl("https://pokemon.gameinfo.io/en/tools/evolution-calculator")
+        webw.loadUrl("https://pokemon.gameinfo.io/en/tools/evolution-calculator")
     }
 }
--- app/src/main/java/com/ozhan/mustafa/pokemongoeye/NotificationListener.kt
+++ app/src/main/java/com/ozhan/mustafa/pokemongoeye/NotificationListener.kt
@@ -1,20 +1,19 @@
 package com.ozhan.mustafa.pokemongoeye
 
+import android.annotation.SuppressLint
 import android.annotation.TargetApi
 import android.content.Context
-import android.content.Intent
 import android.os.Build
-import android.os.Bundle
-import android.os.IBinder
 import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
 import android.util.Log
 
+@SuppressLint("Registered", "OverrideAbstract")
 /**
  * Created by Mustafa Özhan on 7/28/16.
  */
 class NotificationListener : NotificationListenerService() {
-    internal lateinit var context: Context
+    private lateinit var context: Context
 
     override fun onCreate() {
         super.onCreate()
@@ -22,24 +21,17 @@ class NotificationListener : NotificationListenerService() {
 
     }
 
-    override fun onBind(intent: Intent): IBinder? {
-        return super.onBind(intent)
-    }
-
     @TargetApi(Build.VERSION_CODES.KITKAT)
     override fun onNotificationPosted(sbn: StatusBarNotification) {
         super.onNotificationPosted(sbn)
 
-        val NT_pack = sbn.packageName
-        val NT_ticker = sbn.notification.tickerText.toString()
-        val NT_extras = sbn.notification.extras
-        val NT_title = NT_extras.getString("android.title")
-        val NT_text = NT_extras.getCharSequence("android.text")!!.toString()
-        Log.d("Yeni Bildirim: ", "Package: $NT_pack, Title: $NT_title, Text: $NT_text")
+        val ntPack = sbn.packageName
+        val ntTicker = sbn.notification.tickerText.toString()
+        val ntExtras = sbn.notification.extras
+        val ntTitle = ntExtras.getString("android.title")
+        val ntText = ntExtras.getCharSequence("android.text")!!.toString()
+        Log.d("Yeni Bildirim: ", "Package: $ntPack, Title: $ntTitle, Text: $ntText")
 
     }
 
-    override fun onNotificationRemoved(sbn: StatusBarNotification) {
-        super.onNotificationRemoved(sbn)
-    }
 }
